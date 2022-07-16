6c0df776937303935a16ede7f443edf72315a2a1
Shirleen <gathungushirleen@gmail.com>
2022-07-16 09:07:57 +0300

    added Picasso dependencies

--- app/src/main/java/com/example/mycontacts/ContactsRVAdapter.kt
+++ app/src/main/java/com/example/mycontacts/ContactsRVAdapter.kt
@@ -6,19 +6,30 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mycontacts.databinding.ContactListItemBinding
+import com.squareup.picasso.NetworkPolicy
+import com.squareup.picasso.Picasso
 
 class ContactsRVAdapter(var contactList: List<Contact>):RecyclerView.Adapter<ContactsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
-        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.contact_list_item,parent,false)
-        return ContactsViewHolder(itemView)
+        var binding = ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        var contactsViewHolder = ContactsViewHolder(binding)
+        return contactsViewHolder
     }
 
     override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
     var currentContact = contactList.get(position)
-        holder.tvName.text =currentContact.name
-        holder.tvPhoneNumber.text =currentContact.phoneNumber
-        holder.tvEmailAddress.text =currentContact.email
-        holder.tvAddress.text =currentContact.address
+        holder.binding.tvName.text =currentContact.name
+        holder.binding.tvPhoneNumber.text =currentContact.phoneNumber
+        holder.binding.tvEmailAddress.text =currentContact.email
+        holder.binding.tvAddress.text =currentContact.address
+        Picasso.get().load(currentContact.image)
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .error(R.drawable.ic_baseline_error_outline_24)
+            .resize(150,150)
+            .centerCrop()
+            .networkPolicy(NetworkPolicy.OFFLINE)
+            .into(holder.binding.ivContact)
     }
 
     override fun getItemCount(): Int {
@@ -26,11 +37,6 @@ class ContactsRVAdapter(var contactList: List<Contact>):RecyclerView.Adapter<Con
     }
 }
 
-class ContactsViewHolder(itemView:View):
-    RecyclerView.ViewHolder(itemView){
-    var tvName = itemView.findViewById<TextView>(R.id.tvName)
-    var tvPhoneNumber = itemView.findViewById<TextView>(R.id.tvPhoneNumber)
-    var tvEmailAddress = itemView.findViewById<TextView>(R.id.tvEmailAddress)
-    var tvAddress = itemView.findViewById<TextView>(R.id.tvAddress)
-    var ivContact = itemView.findViewById<ImageView>(R.id.ivContact)
+class ContactsViewHolder(var binding: ContactListItemBinding): RecyclerView.ViewHolder(binding.root){
+
 }
--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -15,11 +15,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun displayContacts(){
-        var contact1 =Contact("Leila","+2547890654890","leila@gmail.com","Kilimani890","")
-        var contact2 =Contact("Maria","+254734567890","maria@gmail.com","Housepark","")
-        var contact3 =Contact("Jemeika","+254798765432","jemeika@gmail.com","Maldred hills","")
-        var contact4 =Contact("Paula","+254765432143","paula@gmail.com","Narok","")
-        var contact5 =Contact("Vienna","+254890765432","vienna@gmail.com","Mulberry Heights","")
+        var contact1 =Contact("Leila","+2547890654890","leila@gmail.com","Kilimani890","https://images.pexels.com/photos/936048/pexppppppppels-photo-936048.jpeg?auto=compress&cs=tinysrgb&w=400")
+        var contact2 =Contact("Maria","+254734567890","maria@gmail.com","Housepark","https://images.pexels.com/photos/1820919/pexels-photo-1820919.jpeg?auto=compress&cs=tinysrgb&w=400")
+        var contact3 =Contact("Jemeika","+254798765432","jemeika@gmail.com","Maldred hills","https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&w=400")
+        var contact4 =Contact("Paula","+254765432143","paula@gmail.com","Narok","https://images.pexels.com/photos/1300574/pexels-photo-1300574.jpeg?auto=compress&cs=tinysrgb&w=400")
+        var contact5 =Contact("Vienna","+254890765432","vienna@gmail.com","Mulberry Heights","https://images.pexels.com/photos/175696/pexels-photo-175696.jpeg?auto=compress&cs=tinysrgb&w=400")
 
         var contactsList=listOf(contact1,contact2,contact3,contact4,contact5)
         var contactsAdapter = ContactsRVAdapter(contactsList)
